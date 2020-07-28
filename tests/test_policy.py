38693d9a6dfec35535b596f684427b9165b919d8
tradel <todd@radel.us>
2020-07-28 17:09:48 -0400

    reverse params in calls to assertEqual()

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -6,25 +6,24 @@ from banyan.model import API_VERSION
 class PolicyParserTest(unittest.TestCase):
     def test_parse_web_policy(self):
         p: Policy = Policy.Schema().loads(open("tests/data/policy_web.json").read())
-        self.assertEqual(p.apiVersion, API_VERSION)
-        self.assertEqual(p.kind, Policy.KIND)
-        self.assertEqual(p.metadata.name, "jupyter-allow")
-        self.assertEqual(p.metadata.tags.template, Tags.TEMPLATE_USER)
-        self.assertEqual(p.metadata.tags.template, p.type)
-        self.assertEqual(
-            p.spec.options.disable_tls_client_authentication, True)
-        self.assertEqual(p.spec.options.l7_protocol, "http")
-        self.assertEqual(len(p.spec.access), 1)
+        self.assertEqual(API_VERSION, p.apiVersion)
+        self.assertEqual(Policy.KIND, p.kind)
+        self.assertEqual("jupyter-allow", p.metadata.name)
+        self.assertEqual(Tags.TEMPLATE_USER, p.metadata.tags.template)
+        self.assertEqual(p.type, p.metadata.tags.template)
+        self.assertEqual(True, p.spec.options.disable_tls_client_authentication)
+        self.assertEqual("http", p.spec.options.l7_protocol)
+        self.assertEqual(1, len(p.spec.access))
         a0 = p.spec.access[0]
-        self.assertEqual(a0.rules.conditions.trust_level,
-                         Conditions.TRUST_LEVEL_LOW)
-        self.assertEqual(a0.roles, ["ANY"])
-        self.assertEqual(len(a0.rules.l7_access), 1)
+        self.assertEqual(Conditions.TRUST_LEVEL_LOW, a0.rules.conditions.trust_level                         )
+        self.assertEqual(["ANY"], a0.roles)
+        self.assertEqual(1, len(a0.rules.l7_access))
         r0 = a0.rules.l7_access[0]
-        self.assertEqual(r0.resources, ["*"])
-        self.assertEqual(r0.actions, ["*"])
+        self.assertEqual(["*"], r0.resources)
+        self.assertEqual(["*"], r0.actions)
 
     def test_parse_info(self):
         i: PolicyInfo = PolicyInfo.Schema().loads((open("tests/data/policyinfo.json").read()))
-        self.assertEqual(i.name, "nginx-stage-mesh")
-        self.assertEqual(i.policy.name(), "nginx-stage-mesh")
+        self.assertEqual("nginx-stage-mesh", i.name)
+        self.assertEqual("nginx-stage-mesh", i.policy.name)
+        self.assertEqual("0e599ef2-a9a3-45a2-855b-7c30e80f083f", str(i.id))
