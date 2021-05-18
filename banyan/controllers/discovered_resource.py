5bbf1a412f0d64421f7362be859c3e8237c74ffb
Tarun Desikan <tarun@banyansecurity.io>
2021-05-18 15:42:52 -0700

    dr - sync - add azure

--- banyan/controllers/discovered_resource.py
+++ banyan/controllers/discovered_resource.py
@@ -88,7 +88,7 @@ class DiscoveredResourceController(Controller):
         print(info)
 
 
-    @ex(help='sync discovered_resources with AWS',
+    @ex(help='sync discovered_resources with AWS IaaS',
         arguments=[
             (['resource_type'],
             {
@@ -105,7 +105,7 @@ class DiscoveredResourceController(Controller):
             })
 
         ])
-    def workflow_aws(self):
+    def sync_aws(self):
         try:
             from banyan.ext.aws.ec2 import Ec2Controller, Ec2Model
         except Exception as ex:
@@ -131,21 +131,76 @@ class DiscoveredResourceController(Controller):
                 }
                 res_tags.append(res_tag)
 
-            res = DiscoveredResource(instance.cloud_provider,
-                                     instance.region,
-                                     instance.resource_id,
-                                     instance.resource_name,
-                                     instance.resource_type,
-                                     instance.public_ip,
-                                     instance.private_ip,
-                                     res_tags
-                                     )
+            res = DiscoveredResource(
+                instance.cloud_provider,
+                instance.region,
+                instance.id,
+                instance.name,
+                instance.type,
+                instance.public_ip,
+                instance.private_ip,
+                res_tags
+            )
             self.app.render(DiscoveredResource.Schema().dump(res), handler='json')
             info = self._client.discovered_resources.create(res)
             print('\n-->', info)
             sleep(0.05)
 
-        print('\n--> AWS workflow successful.')
+        print('\n--> Sync with AWS successful.')
+
+
+    @ex(help='sync discovered_resources with Azure IaaS',
+        arguments=[
+            (['resource_type'],
+            {
+                'help': 'Type of Azure Resource - VM | ALL.'
+            }),
+            (['tag_name'],
+            {
+                'help': 'Only sync resources with specific tag name'
+            })
+        ])
+    def sync_azure(self):
+        try:
+            from banyan.ext.azure.vm import VmController, VmModel
+        except Exception as ex:
+            raise NotImplementedError("Azure SDK not configured correctly > %s" % ex.args[0])
+
+        Base.wait_for_input('Getting list of Azure Resources')
+        vm = VmController()
+        instances = vm.list(self.app.pargs.tag_name)
+        results = list()
+        for instance in instances:
+            allvars = vars(copy.copy(instance))
+            allvars['tags'] = len(allvars['tags'])
+            results.append(allvars)
+        self.app.render(results, handler='tabulate', headers='keys', tablefmt='simple')
+
+        Base.wait_for_input('Syncing into Discovered Resource')
+        for instance in instances:
+            res_tags = []
+            for key in instance.tags:
+                res_tag = {
+                    'name': key,
+                    'value': instance.tags[key]
+                }
+                res_tags.append(res_tag)            
+            res = DiscoveredResource(
+                instance.cloud_provider,
+                instance.location,
+                instance.id,
+                instance.name,
+                instance.type,
+                instance.public_ip,
+                instance.private_ip,
+                res_tags
+            )
+            self.app.render(DiscoveredResource.Schema().dump(res), handler='json')
+            info = self._client.discovered_resources.create(res)
+            print('\n-->', info)
+            sleep(0.05)
+
+        print('\n--> Sync with Azure successful.')
 
 
     #TODO-API: argument should be UDID not tag_name
