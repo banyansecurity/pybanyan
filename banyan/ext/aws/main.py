8efe5467e9e9ea5bf91016881429f47ec3ff63e9
Tarun Desikan <tarun@banyansecurity.io>
2021-10-08 20:55:54 -0700

    aws - elb as well

--- banyan/ext/aws/main.py
+++ banyan/ext/aws/main.py
@@ -61,7 +61,7 @@ class Ec2Controller:
                     tags = inst.get('Tags')
                 )
 
-                # resource name
+                # EC2 resource name in tag called Name
                 for tag in res.tags:
                     if tag['Key'] == 'Name':
                         res.name = tag['Value']
@@ -71,6 +71,7 @@ class Ec2Controller:
                 if check_security_groups:
                     security_groups = inst.get('SecurityGroups')
                     listeners = []
+                    valid_ports = []
                     for security_group in security_groups:
                         sg_details = client.describe_security_groups(GroupIds=[security_group['GroupId']])
                         for sg_detail in sg_details.get('SecurityGroups'):
@@ -78,15 +79,12 @@ class Ec2Controller:
                             if inbound_rules:
                                 for inbound_rule in inbound_rules:
                                     listeners.append({
-                                        'port': inbound_rule.get("FromPort"), 
-                                        'protocol': inbound_rule.get("IpProtocol")
-                                    })
-                    
-                    valid_ports = []
+                                        'port': inbound_rule.get('FromPort'), 
+                                        'protocol': inbound_rule.get('IpProtocol')
+                                    })                    
                     for listener in listeners:
                         if listener['port'] and listener['port'] > 0:
                             valid_ports.append(f'{listener["port"]}/{listener["protocol"]}')
-                    
                     res.ports = ','.join(valid_ports)
 
                 instances.append(res)
@@ -139,11 +137,134 @@ class RdsController:
         return instances
 
 
+class ElbController:
+    TYPE_V1 = 'elb'     # classic
+    TYPE_V2 = 'elbv2'   # current
+
+    def list(self, tag_name: str = None, tag_values: list = ['*']):
+        try:
+            session = boto3.session.Session()
+            sts = session.client('sts')
+            client_v1 = session.client(ElbController.TYPE_V1)
+            client_v2 = session.client(ElbController.TYPE_V2)
+        except Exception as ex:
+            print('BotoError (AWS SDK) > %s' % ex.args[0])
+            raise        
+
+        caller_identity  = sts.get_caller_identity()
+
+        filters = []
+        if tag_name is not None:
+            filters.append({
+                'Name': 'tag:%s' % tag_name,
+                'Values': tag_values
+            })
+        
+        describe_v1_lbs = client_v1.describe_load_balancers(PageSize=100)
+        print(describe_v1_lbs)
+        describe_v2_lbs = client_v2.describe_load_balancers(PageSize=100)
+        print(describe_v2_lbs)
+
+        instances: List[AwsResourceModel] = list()
+
+        v1_lbs = describe_v1_lbs.get('LoadBalancerDescriptions')
+        for v1_lb in v1_lbs:
+            res = AwsResourceModel(
+                    account = caller_identity.get('Account'),
+                    region = session.region_name,
+                    type = ElbController.TYPE_V1,
+                    id = v1_lb.get('LoadBalancerName'),
+                    name = v1_lb.get('LoadBalancerName'),
+                    public_dns_name = v1_lb.get('DNSName')  # public DNS, resolves to public or private IP
+            )
+
+            listeners = []
+            valid_ports = []
+            for ld in v1_lb.get('ListenerDescriptions'):
+                ld_l = ld.get('Listener')
+                listeners.append({
+                    'port': ld_l.get('LoadBalancerPort'),
+                    'protocol': ld_l.get('Protocol')
+                })
+            for listener in listeners:
+                if listener['port'] and listener['port'] > 0:
+                    valid_ports.append(f'{listener["port"]}/{listener["protocol"]}')            
+            res.ports = ','.join(valid_ports)
+
+            describe_tags = client_v1.describe_tags(LoadBalancerNames=[v1_lb.get('LoadBalancerName')])
+            tags = describe_tags.get('TagDescriptions')[0].get('Tags')
+            res.tags = tags
+
+            # add a tag for internet-facing | internal
+            res.tags.append({
+                "Key": "Scheme",
+                "Value": v1_lb.get('Scheme')
+            })
+
+            # implement tag filtering ourselves, don't implement tag_values
+            if not tag_name:
+                instances.append(res)
+            else:
+                for tag in res.tags:
+                    if tag['Key'] == tag_name:
+                        # assume tag_values == ['*']:
+                        instances.append(res)
+                        break
+
+        v2_lbs = describe_v2_lbs.get('LoadBalancers')
+        for v2_lb in v2_lbs:
+            res = AwsResourceModel(
+                    account = caller_identity.get('Account'),
+                    region = session.region_name,
+                    type = ElbController.TYPE_V2,
+                    id = v2_lb.get('LoadBalancerArn'),
+                    name = v2_lb.get('LoadBalancerName'),
+                    public_dns_name = v2_lb.get('DNSName')  # public DNS, resolves to public or private IP
+            )
+
+            listeners = []
+            valid_ports = []
+            describe_listeners = client_v2.describe_listeners(LoadBalancerArn=v2_lb.get('LoadBalancerArn'))
+            for ld in describe_listeners.get('Listeners'):
+                listeners.append({
+                    'port': ld.get('Port'),
+                    'protocol': ld.get('Protocol')
+                })
+            for listener in listeners:
+                if listener['port'] and listener['port'] > 0:
+                    valid_ports.append(f'{listener["port"]}/{listener["protocol"]}')            
+            res.ports = ','.join(valid_ports)
+
+            describe_tags = client_v2.describe_tags(ResourceArns=[v2_lb.get('LoadBalancerArn')])
+            tags = describe_tags.get('TagDescriptions')[0].get('Tags')
+            res.tags = tags
+
+            # add a tag for internet-facing | internal
+            res.tags.append({
+                "Key": "Scheme",
+                "Value": v2_lb.get('Scheme')
+            })
+
+            # implement tag filtering ourselves, don't implement tag_values
+            if not tag_name:
+                instances.append(res)
+            else:
+                for tag in res.tags:
+                    if tag['Key'] == tag_name:
+                        # assume tag_values == ['*']:
+                        instances.append(res)
+                        break
+                    
+        return instances
+
 
 
 if __name__ == '__main__':
     #ec2 = Ec2Controller()
     #my_instances = ec2.list('banyan:discovery', ['true'], True)
-    rds = RdsController()
-    my_instances = rds.list('banyan:discovery')
+    #rds = RdsController()
+    #my_instances = rds.list('banyan:discovery')
+    elb = ElbController()
+    my_instances = elb.list('banyan:discovery')
+
     print(my_instances)
