04d7b37381df543405327431c705cf9afa484176
Tarun Desikan <tarun@banyansecurity.io>
2021-10-09 21:50:37 -0700

    azure - refactor ctrl

--- banyan/ext/azure/vm.py
+++ banyan/ext/azure/vm.py
@@ -1,6 +1,6 @@
 from typing import List, ClassVar, Type, Optional, Union
-from dataclasses import dataclass
-import os
+from dataclasses import dataclass, field
+import os, json
 
 try: 
     from azure.identity import DefaultAzureCredential
@@ -12,74 +12,78 @@ except ImportError as ex:
     raise
 
 @dataclass
-class VmModel:
-    class Meta:
-        help = "Azure VM Model"
-    
+class AzureResourceModel:
+    subscription: str   # Azure Subscription is within an Account 
+    resource_group: str          # Azure Resource Group
     location: str
-    group: str
     id: str
-    name: str
-    public_ip: str
-    private_ip: str
-    tags: List
-    cloud_provider: str = 'Azure'
-    type: str = 'Microsoft.Compute/virtualMachines'
+    name: str = ''
+    public_dns_name: str = ''
+    public_ip: str = ''
+    private_dns_name: str = ''
+    private_ip: str = ''
+    ports: str = ''
+    tags: List = field(default_factory=list)
 
-class VmController:
-    class Meta:
-        help = "Azure VM Controller"
+    PROVIDER = 'AZURE'
 
-    #TODO: support more filters - resource_group, location, etc
-    def list(self, tag_name: str = None):
+class AzureController:
+    def __init__(self, location: str = None, tag_name: str = None):
         try:
-            credential = DefaultAzureCredential()
-            subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
-            resource_client = ResourceManagementClient(credential, subscription_id)
-            compute_client =  ComputeManagementClient(credential, subscription_id)
-            network_client = NetworkManagementClient(credential,subscription_id)
+            self._credential = DefaultAzureCredential()
+            self._subscription = os.environ["AZURE_SUBSCRIPTION_ID"]
+            self._resource_client = ResourceManagementClient(self._credential, self._subscription)
         except Exception as ex:
             print('AzureSDKError > %s' % ex.args[0])
             raise
+        self._resource_client.resource_groups.list()
+        self._location = location
+        self._tag_name = tag_name
 
-        instances: List[VmModel] = list()
-        vm_list = compute_client.virtual_machines.list_all()
+
+    def list_vm(self):
+        compute_client =  ComputeManagementClient(self._credential, self._subscription)
+        network_client = NetworkManagementClient(self._credential, self._subscription)
+
+        vm_list = list()
+        if self._location:
+            vm_list = compute_client.virtual_machines.list_by_location(self._location)
+        else:
+            vm_list = compute_client.virtual_machines.list_all()
+
+        instances: List[AzureResourceModel] = list()
         for vm in list(vm_list):
-            if tag_name is not None and not vm.tags.get(tag_name):
-                continue
-
-            ni_reference = vm.network_profile.network_interfaces[0]
-            ni_reference = ni_reference.id.split('/')
-            ni_group = ni_reference[4]
+            print(json.dumps(vm.as_dict(), indent=4))
+            vm_reference = vm.id.split('/')
+            vm_rg = vm_reference[4]
+            ni_reference = vm.network_profile.network_interfaces[0].id.split('/')
             ni_name = ni_reference[8]
 
-            net_interface = network_client.network_interfaces.get(ni_group, ni_name)
-            pub_ip_reference = net_interface.ip_configurations[0].public_ip_address
-            pub_ip_reference = pub_ip_reference.id.split('/')
-            pub_ip_group = pub_ip_reference[4]
-            pub_ip_name = pub_ip_reference[8]
-
-            public_ip = network_client.public_ip_addresses.get(pub_ip_group, pub_ip_name)
-            public_ip = public_ip.ip_address
+            net_interface = network_client.network_interfaces.get(vm_rg, ni_name)
+            print(json.dumps(net_interface.as_dict(), indent=4))
             
             private_ip = net_interface.ip_configurations[0].private_ip_address
 
-            res = VmModel(
-                vm.location,
-                ni_group,
-                vm.id,
-                vm.name,
-                public_ip,
-                private_ip,
-                vm.tags
+            res = AzureResourceModel(
+                subscription = self._subscription,
+                resource_group = vm_rg,
+                location = vm.location,
+                id = vm.id,
+                name = vm.name,
+                private_ip = private_ip,
+                tags = vm.tags
             )
+
+            if self._tag_name and vm.tags and not vm.tags.get(self._tag_name):
+                continue
+
             instances.append(res)
 
         return instances
 
 
 if __name__ == '__main__':
-    vmc = VmController()
-    my_vms = vmc.list('Env')
+    azr = AzureController()
+    my_vms = azr.list_vm()
     print(my_vms)
 
