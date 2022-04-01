30b612dcce1f71d0624c6ed6a8b4f252ea5c53cc
Tarun Desikan <tarun@banyansecurity.io>
2022-03-31 23:19:27 -0700

    api - service - filter for web/infra

--- banyan/api/service_web.py
+++ banyan/api/service_web.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Union, Iterable, Any, Callable
+
 from banyan.api.base import ApiBase
 from banyan.model.policy import PolicyInfo, PolicyInfoOrName, PolicyAttachInfo
 from banyan.model.service import ServiceInfo, Service, ServiceInfoOrName
@@ -12,6 +14,17 @@ class ServiceWebAPI(ApiBase):
         insert_uri = '/insert_registered_service'
         uri_param = 'ServiceID'
         obj_name = 'service'
+
+    def list(self, params: Dict[str, Any] = None) -> list:
+        list_func = self._client.api_request
+        response_json = list(list_func('GET', self.Meta.list_uri, params=params))
+        data: List[ServiceInfo] = self.Meta.info_class.Schema().load(response_json, many=True)        
+        filtered_data = []
+        for svc in data:
+            if svc.oidc_enabled:
+                filtered_data.append(svc)
+        self._build_cache(filtered_data)
+        return filtered_data
 
     def enable(self, service: ServiceInfoOrName) -> str:
         service = self.find(service)
@@ -34,9 +47,6 @@ class ServiceWebAPI(ApiBase):
     def detach(self, service: ServiceInfoOrName, policy: PolicyInfoOrName) -> str:
         from banyan.api.policy import PolicyAPI
         return PolicyAPI(self._client).detach(policy, service)
-
-    def test(self, service: ServiceInfoOrName) -> None:
-        pass
 
     def attached_policy(self, service: ServiceInfoOrName) -> PolicyInfo:
         from banyan.model.policy import PolicyAttachInfo
